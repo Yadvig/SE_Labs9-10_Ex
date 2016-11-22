d308ca859ce0c463f684bf18e2f1bd15d76ef9c1
Viktor <viktor.v.antoniuk@gmail.com>
2016-11-22 09:09:26 +0200

    Initial

---
+++ SE/Controllers/BankManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SE.Models;
+
+namespace SE.Controllers
+{
+    public class BankManager
+    {
+        private Bank<Account> bank;
+        public BankManager(Bank<Account> Bank)
+        {
+            bank = Bank;
+        }
+        public void OpenAccount()
+        {
+            Console.WriteLine("Укажите сумму для создания счета:");
+
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Выберите тип счета: 1. До востребования 2. Депозит");
+            AccountType accountType;
+
+            int type = Convert.ToInt32(Console.ReadLine());
+
+            if (type == 2)
+                accountType = AccountType.Deposit;
+            else
+                accountType = AccountType.Ordinary;
+
+            bank.Open(accountType,
+                sum,
+                AddSumHandler,  // обработчик добавления средств на счет
+                WithdrawSumHandler, // обработчик вывода средств
+                (o, e) => Console.WriteLine(e.Message), // обработчик начислений процентов в виде лямбда-выражения
+                CloseAccountHandler, // обработчик закрытия счета
+                OpenAccountHandler); // обработчик открытия счета
+        }
+
+        public void Withdraw()
+        {
+            Console.WriteLine("Укажите сумму для вывода со счета:");
+
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите id счета:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            bank.Withdraw(sum, id);
+        }
+
+        public void Put()
+        {
+            Console.WriteLine("Укажите сумму, чтобы положить на счет:");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите Id счета:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            bank.Put(sum, id);
+        }
+
+        public void CloseAccount()
+        {
+            Console.WriteLine("Введите id счета, который надо закрыть:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            bank.Close(id);
+        }
+        public void CalculatePercentage()
+        {
+            bank.CalculatePercentage();
+        }
+        // обработчики событий класса Account
+        // обработчик открытия счета
+        private static void OpenAccountHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        // обработчик добавления денег на счет
+        private static void AddSumHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        // обработчик вывода средств
+        private static void WithdrawSumHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            if (e.Sum > 0)
+                Console.WriteLine("Идем тратить деньги");
+        }
+        // обработчик закрытия счета
+        private static void CloseAccountHandler(object sender, AccountEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
