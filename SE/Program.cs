0fd5e854af9ed7c8c9c197ffc7979d3f84fdb30c
Viktor <viktor.v.antoniuk@gmail.com>
2016-11-22 09:20:40 +0200

    View added

--- SE/Program.cs
+++ SE/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SE.Models;
 using SE.Controllers;
+using SE.Views;
 
 namespace SE
 {
@@ -14,50 +15,9 @@ namespace SE
         {
 
             BankManager manager = new BankManager(new Bank<Account>("ЮнитБанк"));
-            bool alive = true;
-            while (alive)
-            {
-                ConsoleColor color = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.DarkBlue; // выводим список команд зеленым цветом
-                Console.WriteLine("1. Открыть счет \t 2. Вывести средства  \t 3. Добавить на счет");
-                Console.WriteLine("4. Закрыть счет \t 5. Пропустить день \t 6. Выйти из программы");
-                Console.WriteLine("Введите номер пункта:");
-                Console.ForegroundColor = color;
-                try
-                {
-                    int command = Convert.ToInt32(Console.ReadLine());
-
-                    switch (command)
-                    {
-                        case 1:
-                            manager.OpenAccount();
-                            break;
-                        case 2:
-                            manager.Withdraw();
-                            break;
-                        case 3:
-                            manager.Put();
-                            break;
-                        case 4:
-                            manager.CloseAccount();
-                            break;
-                        case 5:
-                            break;
-                        case 6:
-                            alive = false;
-                            continue;
-                    }
-                    manager.CalculatePercentage();
-                }
-                catch (Exception ex)
-                {
-                    // выводим сообщение об ошибке красным цветом
-                    color = Console.ForegroundColor;
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(ex.Message);
-                    Console.ForegroundColor = color;
-                }
-            }
+            ConsoleView view = new ConsoleView(manager);
+            view.ViewEngine();
+           
         }
        
     }
