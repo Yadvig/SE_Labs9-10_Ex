db13a4a060e186b3f34ef2158e9be1d4cdf4d400
Viktor <viktor.v.antoniuk@gmail.com>
2016-11-24 12:56:30 +0200

    Added Models.AccountContext.cs

---
+++ SE/Models/AccountContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SE.Models
+{
+    class AccountContext: DbContext
+    {
+        public AccountContext(): base ("DbConnetcion") { }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
